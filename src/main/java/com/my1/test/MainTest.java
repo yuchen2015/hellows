7c4cf68eb1630fea186f3882e66565d219a41447
yu.chen <yu.chen@ctrip.com>
2015-06-09 17:13:33 +0800

    2222

---
+++ src/main/java/com/my1/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.my1.test;
+
+/**
+ * Created by yu.chen on 2015/6/9.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hehe");
+    }
+}
